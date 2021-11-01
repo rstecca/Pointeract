afc7d16fccc53f382a769cfdf4083f5c0d34a71c
rstecca <inceneritoreintrepido@gmail.com>
2021-11-01 18:44:59 +0000

    PointableEvents exposes events to inspector

---
+++ Runtime/Scripts/PointableEvents.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Pointeract
+{
+
+    public class PointableEvents : MonoBehaviour, IPointable, IInteractable
+    {
+        [SerializeField] UnityEvent OnInteract, OnPointEnter, OnPointExit, OnPointStay;
+
+        //void Start()
+        //{
+
+        //}
+
+        void IInteractable.OnInteract()
+        {
+            OnInteract.Invoke();
+        }
+
+        void IPointable.OnPointerEnter()
+        {
+            OnPointEnter.Invoke();
+        }
+
+        void IPointable.OnPointerExit()
+        {
+            OnPointExit.Invoke();
+        }
+
+        void IPointable.OnPointerStay()
+        {
+            OnPointStay.Invoke();
+        }
+
+    }
+
+}
